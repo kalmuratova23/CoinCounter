e3c805216671b1d3357fe38d7fab4566684866c6
Akerke <yerlankyzyyy@mail.ru>
2024-12-19 16:57:41 +0500

    updated

--- CoinCounter/src/App.vue
+++ CoinCounter/src/App.vue
@@ -1,47 +1,85 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+ <h1>Crypto</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+ <Input :changeAmount = "changeAmount" :convert="convert" />
+  <p v-if="error != ''">{{ error }}</p>
+  <p v-if="result != 0" class="result-text">{{ result }}</p>
+ <div className="selectors">
+  <Selector :setCrypto="setCryptoFirst" />
+  <Selector :setCrypto="setCryptoSecond" />
+ </div>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
+
+<script>
+import Input from './components/Input.vue';
+import Selector from './components/Selector.vue';
+import CryptoConvert from 'crypto-convert';
+
+const convert = new CryptoConvert();
+
+export default{
+  components:{
+    Input, Selector
+  },
+  data(){
+    return{
+      amount: 0,
+      cryptoFirst: '',
+      cryptoSecond: '',
+      error:'',
+      result:0
+    }
+  },
+  methods:{
+    changeAmount(val){
+      this.amount = val
+    },
+    setCryptoFirst(val){
+      this.cryptoFirst = val
+    },
+    setCryptoSecond(val){
+      this.cryptoSecond = val
+    },
+    async convert(){
+      if(this.amount <= 0){
+        this.error = 'Enter number greater than 0';
+        return;
+      }else if(this.cryptoFirst == this.cryptoSecond){
+        this.error = 'Enter different cryptos';
+        return;
+      }else if(this.cryptoFirst == '' || this.cryptoSecond == ''){
+        this.error = 'Select crypto';
+        return;
+      }
+      this.error = '';
+      await convert.ready();
+      
+      if(this.cryptoFirst == 'BTC' && this.cryptoSecond == 'ETH')
+      this.result = convert.BTC.ETH(this.amount);
+      else if(this.cryptoFirst == 'BTC' && this.cryptoSecond == 'USDT')
+      this.result = convert.BTC.USDT(this.amount);
+      else if(this.cryptoFirst == 'ETH' && this.cryptoSecond == 'BTC')
+      this.result = convert.ETH.BTC(this.amount);
+      else if(this.cryptoFirst == 'ETH' && this.cryptoSecond == 'USDT')
+      this.result = convert.ETH.USDT(this.amount);
+      else if(this.result == 'USDT' && this.cryptoSecond == 'BTC')
+      this.result = convert.USDT.BTC(this.amount);
+      else if(this.result == 'USDT' && this.cryptoSecond == 'ETH')
+      this.result = convert.USDT.ETH(this.amount);
+    }
+  }
+}
+</script>
+
+
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.selectors{
+  display: flex;
+  justify-content: space-around;
+  width: 700px;
+  margin: 0 auto;
 }
 </style>
---
+++ CoinCounter/src/components/Input.vue
@@ -0,0 +1,46 @@
+<template>
+<input min="0" type="number" @input="changeAmount($event.target.value)" placeholder="Enter a number"> <br>
+<button @click="convert()" >Convert</button>
+</template>
+  
+<script>
+   export default{
+    props:{
+        changeAmount:{
+            type:Function,
+            required: true
+        },
+        convert:{
+            type: Function,
+            required:true
+        }
+    }
+   }
+</script>
+   
+<style scoped>
+input{
+    outline: none;
+    width: 500px;
+    position: relative;
+    top: -25px;
+    font-size: 1.2em;
+    border-radius: 3px;
+    border: 0;
+    padding: 10px 15px;
+    background:#fafafa;
+    color: #333;
+}
+button{
+    position: relative;
+    top: -20px;
+    padding: 15px 20px;
+    color: #fff;
+    text-transform: uppercase;
+    cursor: pointer;
+    background: #1a032d;
+    border: 0;
+    border-radius: 5px;
+}
+</style>
+   
